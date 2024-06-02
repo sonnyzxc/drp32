cb7adfca2f2670ffb65e911a1b6c1ada65ed9df8
Hoang Nguyen <hn622@ic.ac.uk>
2024-06-02 03:28:02 +0100

    fix(BE-endpoints): [@hxcuber] Fix call to database to include familyID.

--- be_drp32/api/internal/repository/chore/getFamilyChores.go
+++ be_drp32/api/internal/repository/chore/getFamilyChores.go
@@ -12,8 +12,18 @@ import (
 
 func (i impl) GetFamilyChores(ctx context.Context, familyID int64) (model.Chores, error) {
 	query := fmt.Sprintf(
-		"SELECT * FROM %s",
+		"SELECT * FROM %s "+
+			"WHERE %s IN ( "+
+			"SELECT %s FROM %s "+
+			"WHERE %s=%d "+
+			") AND %s=FALSE",
 		orm.TableNames.Chores,
+		orm.ChoreTableColumns.AssignedTo,
+		orm.UserTableColumns.UserID,
+		orm.TableNames.Users,
+		orm.UserTableColumns.FamilyID,
+		familyID,
+		orm.ChoreTableColumns.Completed,
 	)
 
 	var chores model.Chores
